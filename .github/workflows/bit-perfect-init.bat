e522cf8af94109e7e3d437f8eb75ab0d397ab8d6
aurora <retrogreensnake@gmail.com>
2021-10-22 15:31:59 +0300

    change batch files to output text over having comments

--- .github/workflows/bit-perfect-init.bat
+++ .github/workflows/bit-perfect-init.bat
@@ -1,11 +1,11 @@
 @echo off
 cd "..\..\"
 
-rem assemble Z80 sound driver - same on all revisions
+echo assemble Z80 sound driver - same on all revisions
 axm68k /m /k /p "sound\DAC Driver.asm", "sound\DAC Driver.unc"
 IF NOT EXIST "sound\DAC Driver.unc" EXIT 2
 
-rem compress kosinski files - same on all revisions
+echo compress kosinski files - same on all revisions
 for %%f in ("256x256 Mappings\*.unc") do kosinski_compress "%%f" "256x256 Mappings\%%~nf.kos"
 kosinski_compress "Graphics - Compressed\Ending Flowers.unc" "Graphics - Compressed\Ending Flowers.kos"
 kosinski_compress "sound\DAC Driver.unc" "sound\DAC Driver.kos"
--- .github/workflows/bit-perfect.bat
+++ .github/workflows/bit-perfect.bat
@@ -1,18 +1,16 @@
 @echo off
 cd "..\..\"
 
-rem assemble rom
+echo assemble revision %1 rom
 axm68k /e Revision=%1 /m /k /p sonic.asm, %1.bin
 
-rem check for success and fix header
+echo check for success and fix header
 IF NOT EXIST %1.bin EXIT 2
 fixheadr.exe %1.bin
 
-rem check file content
+echo check file content
 fc /b %1.bin s1rev%1.bin > nul
 
-rem delete the built rom
+echo delete the built rom
 del %1.bin
-
-rem return the value of the fc call
 exit errorlevel
