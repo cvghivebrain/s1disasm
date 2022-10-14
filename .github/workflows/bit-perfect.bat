5ca1b1485fdfe9f8c2f69665ad0de39b52c7583e
cvghivebrain <66194501+cvghivebrain@users.noreply.github.com>
2022-10-14 21:17:27 +0100

    Fixed workflow (I think)

--- .github/workflows/bit-perfect.bat
+++ .github/workflows/bit-perfect.bat
@@ -3,6 +3,9 @@
 echo assemble revision %1 rom
 axm68k /e Revision=%1 /m /k /p sonic.asm, %1.bin
 
+echo compress and insert DAC driver
+"DualPCM_Compress.exe" "sound\DAC Driver.unc" "sound\DAC Driver Offset & Size.dat" %1.bin kosinski_compress.exe
+
 echo check for success and fix header
 IF NOT EXIST %1.bin EXIT 2
 fixheadr.exe %1.bin
